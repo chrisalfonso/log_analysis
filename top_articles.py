d54bc709fab3d9c9ad03d321385c270f56916e88
chrisalfonso <chrisalfonso@users.noreply.github.com>
2017-07-08 00:16:42 -0400

    Update for pep8

--- top_articles.py
+++ top_articles.py
@@ -2,7 +2,7 @@ import psycopg2
 
 DBNAME = "news"
 
-db = psycopg2.connect(database=DBNAME)	
+db = psycopg2.connect(database=DBNAME)
 cur = db.cursor()
 query = "SELECT articles.title, views FROM article_views JOIN articles on article_views.path_slug = articles.slug ORDER BY views desc LIMIT 3;"
 cur.execute(query)
@@ -10,7 +10,7 @@ articles = cur.fetchall()
 
 print "Top 3 Articles"
 for article in articles:
-	print article[0], "--", article[1], "views"
+    print article[0], "--", article[1], "views"
 
 db.close()
 
@@ -18,23 +18,23 @@ db.close()
 
 # 1. Get top 3 articles from LOG table. I need to strip out part of the url and just get the slug.
 SELECT path, substring (path from 10) as short_slug, count(*) as views
-FROM log 
-WHERE path not like '/' 
-GROUP BY path 
+FROM log
+WHERE path not like '/'
+GROUP BY path
 ORDER BY views desc limit 3;
 
 # 2. Get title of articles by joining ARTICLES and LOG tables matching on slug
-SELECT articles.title, log.path 
-FROM articles 
+SELECT articles.title, log.path
+FROM articles
 JOIN log on articles.slug = substring (log.path from 10) limit 3;
 
 # Trying a subquery (edit: this works!!!):
 SELECT articles.title, views
 FROM (
-	SELECT path, count(*) as views from log
-	WHERE path not like '/'
-	GROUP BY path
-	) as log_views
+    SELECT path, count(*) as views from log
+    WHERE path not like '/'
+    GROUP BY path
+    ) as log_views
 JOIN articles on articles.slug = substring (log_views.path from 10)
 ORDER BY views desc
 LIMIT 3;
