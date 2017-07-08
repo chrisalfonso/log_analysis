392eded5cb7f1d2283e6299c70c3b39c81fba078
chrisalfonso <chrisalfonso@users.noreply.github.com>
2017-07-08 00:23:11 -0400

    Update for pep8

--- top_authors.py
+++ top_authors.py
@@ -2,9 +2,14 @@ import psycopg2
 
 DBNAME = "news"
 
-db = psycopg2.connect(database=DBNAME)	
+db = psycopg2.connect(database=DBNAME)
 cur = db.cursor()
-query = "SELECT name, sum(views) as total_views FROM (SELECT authors.name, articles.title, article_views.views FROM articles JOIN authors on articles.author = authors.id JOIN article_views on articles.slug = article_views.path_slug	ORDER BY views desc) as t GROUP BY name ORDER BY total_views desc;" 
+query = "SELECT name, sum(views) as total_views \
+(SELECT authors.name, articles.title, article_views.views \
+FROM articles JOIN authors on articles.author = authors.id \
+JOIN article_views on articles.slug = article_views.path_slug \
+ORDER BY views desc) as t \
+GROUP BY name ORDER BY total_views desc;"
 cur.execute(query)
 authors = cur.fetchall()
 
@@ -21,7 +26,7 @@ JOIN authors on articles.author = authors.id
 ORDER BY authors.name;
 
 # I think I'm going to need a view to list the number of article views:
-CREATE VIEW article_views AS 
+CREATE VIEW article_views AS
 SELECT path, substring (path from 10) as path_slug, count(*) as views
 FROM log
 WHERE path like '%/article/%'
@@ -29,19 +34,20 @@ GROUP BY path
 
 (note: leave "ORDER BY views desc" to the outer query)
 
-# 2. Get author names, titles of articles they've written and the views of the individual articles
+# 2. Get author names, titles of articles they've written and \
+    the views of the individual articles
 SELECT authors.name, articles.title, article_views.views FROM articles
-JOIN authors on articles.author = authors.id 
+JOIN authors on articles.author = authors.id
 JOIN article_views on articles.slug = article_views.path_slug
 ORDER BY views desc;
 
 # 3. Building off of #2, aggregate the views and show who has the most!
 SELECT name, sum(views) as total_views
 FROM (
-	SELECT authors.name, articles.title, article_views.views FROM articles
-	JOIN authors on articles.author = authors.id 
-	JOIN article_views on articles.slug = article_views.path_slug
-	ORDER BY views desc) as t
+    SELECT authors.name, articles.title, article_views.views FROM articles
+    JOIN authors on articles.author = authors.id
+    JOIN article_views on articles.slug = article_views.path_slug
+    ORDER BY views desc) as t
 GROUP BY name
 ORDER BY total_views desc;
 
